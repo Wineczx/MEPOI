24ce145192d1b29aaa674a38fbd10024e60e8003
wineczx <63555362+Wineczx@users.noreply.github.com>
2025-04-11 11:56:26 +0800

    Delete data/getbusiness.py

--- data/getbusiness.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-df = pd.read_json('/data/CaiZhuaoXiao/yelp/yelp_academic_dataset_business.json',lines=True)
-state_counts = df['state'].value_counts()
-print(state_counts)
-filtered_df = df[df['state'] == 'PA']
-print(len(filtered_df))
-filtered_df.to_json('/data/CaiZhuaoXiao/yelp/PAA/PA_business.json', orient='records', lines=True)
